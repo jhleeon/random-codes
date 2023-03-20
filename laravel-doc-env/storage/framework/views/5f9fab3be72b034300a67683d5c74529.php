94ea015fb0f68a05d15e87076e3902341787ddc1
jhleeon <jhleon288@gmail.com>
2023-03-20 11:53:23 +0600

    laravel-doc-env-part-1

---
+++ laravel-doc-env/storage/framework/views/5f9fab3be72b034300a67683d5c74529.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Env File</title>
+</head>
+<body>
+    Using env method: <?php echo e($environment); ?>
+
+    <br>
+    Using app's method: <?php echo e($env); ?>
+
+</body>
+</html><?php /**PATH C:\xampp\htdocs\random-codes\laravel-env\resources\views/welcome.blade.php ENDPATH**/ ?>
---
+++ laravel-doc-env/storage/framework/views/768339cf00f4649e9a088c4cc0a7a8ae.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php if(env('APP_ENV') == 'local'): ?>
+        "local environment"
+    <?php endif; ?>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\example-app\resources\views/welcome.blade.php ENDPATH**/ ?>
