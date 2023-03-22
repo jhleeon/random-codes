cefb264fbfd2aeb2b5c463269eef7969e7a2ad83
jhleeon <jhleon288@gmail.com>
2023-03-22 23:33:55 +0600

    react render method

---
+++ react-test-3/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+    <div>
+      <h3>Jahedul Hasan</h3>
+      <p>Developer at softnio
+      <span>Mohakhali</span>
+      </p>
+    </div>
+    );
+  }
+}
+export default App;
