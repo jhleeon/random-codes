7dc93608e904149d189fd55d3c80e126a7aeb6e1
jhleeon <jhleon288@gmail.com>
2023-03-26 15:34:28 +0600

    laravel single auth using breeze & middleware

---
+++ laravel-breeze/resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+ <img src="{{ asset('logo/logo.jpg') }}" width="200px" alt="logo">
---
+++ laravel-middleware/resources/views/notadult.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Not a Valid Member</title>
+</head>
+<body>
+    <div>
+        <p>Sorry, You are not 18+</p>
+        <p>Try Next Year. Thank you</p>
+    </div>
+</body>
+</html>
