952e8995070f4a81a06557baebe2d979a52d324e
jhleeon <jhleon288@gmail.com>
2023-04-07 15:49:51 +0600

    laravel-vite test

---
+++ laravel-vite/laravel-vite/resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- 'resources/css/app.scss', -->
+</head>
+<body>
+
+
+<h1>Hello!<span>SCSS</span></h1>
+
+<div class ="bg-blue-700 text-white">Hello, Vite!</div> 
+    
+</body>
+</html>
